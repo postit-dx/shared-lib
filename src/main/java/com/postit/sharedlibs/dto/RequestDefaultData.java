8f5e562e18749c1b18471d521d09abb63c73893d
Midichi <8282qwe@gmail.com>
2025-03-07 08:59:20 +0900

    Add RequestDefaultData

---
+++ src/main/java/com/postit/sharedlibs/dto/RequestDefaultData.java
@@ -0,0 +1,17 @@
+package com.postit.sharedlibs.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class RequestDefaultData {
+	private String accountId;
+	private String pageId;
+}
